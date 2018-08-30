3293302c4ffad4c6652b4899b690d52ddd673afc
Hoa Newton <hoa@ellevatenetwork.com>
2018-08-30 08:07:05 -0400

    book detail shows when book is clicked from the list

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList(){
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">
-          {book.title}
+        <li
+        key={book.title} 
+        onClick={() => this.props.selectBook(book)}
+        className="list-group-item">
+        {book.title}
         </li>
       );
     });
@@ -31,6 +36,13 @@ function mapStateToProps(state) {
   }
 }
 
-export default connect(mapStateToProps)(BookList);
+// Anthing retured from this function will end up as props on the BookList container
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed to all of our reducers
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+// Promote BookList from a component to a container - it needs to know about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
 // this connect function takes that function and a Component and produces a containter
 // the Container is the Component that is awared of the state that is contained by redux
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
   // this reducer will add a key to the Global application state called 'books' and the value is from BooksReducer
+  books: BooksReducer,
+  activeBook: ActiveBook
 });
 
 
